d048520a426fe0a37350e4907920d85380ec5c8e
Jamily Lima <jamilylima70@gmail.com>
2022-09-06 15:32:37 -0300

    feat:create middlewares adm/auth

---
+++ src/middlewares/adm.middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express'
+import AppDataSource from "../data-source";
+import { AppError } from '../errors/appError';
+
+const admMiddlleware = async (req: Request, res: Response, next: NextFunction) => {
+    
+  const adm = req.body.isAdm
+  
+  if (adm === false) {
+    throw new AppError(403,"Not authorization")
+  } 
+  next();
+
+
+};
+
+export default admMiddlleware;
